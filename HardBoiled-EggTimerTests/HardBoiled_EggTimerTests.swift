ce6314d8c223c3e01c41685206c35bbddd726592
jscheth <sovichea.chet@gmail.com>
2025-03-26 12:29:53 -0700

    Initial Commit

---
+++ HardBoiled-EggTimerTests/HardBoiled_EggTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HardBoiled_EggTimerTests.swift
+//  HardBoiled-EggTimerTests
+//
+//  Created by Jonathan Cheth on 3/26/25.
+//
+
+import Testing
+@testable import HardBoiled_EggTimer
+
+struct HardBoiled_EggTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
